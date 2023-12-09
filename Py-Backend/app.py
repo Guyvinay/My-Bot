67ae23d88d8014347036a6575f1f5120e02a4c3f
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-12-09 12:29:30 +0530

    User-creation-acc-mysql

--- Py-Backend/app.py
+++ Py-Backend/app.py
@@ -6,69 +6,71 @@ import openai
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/mybot'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Set your OpenAI API key
-openai.api_key = ''
+openai.api_key = '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'
 
-class ConversationHistory(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(2000), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100))
+    chats = db.relationship('Chat', backref='users', lazy=True)
 
-# db.create_all()
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    conversations = db.relationship('Conversation', backref='chat', lazy=True)
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
+    prompt = db.Column(db.String(500), nullable=False)
+    response = db.Column(db.String(500), nullable=False)
+
+#Creating modals
 with app.app_context():
     db.create_all()
-# Maintain conversation history
-# conversation_history = []
 
-# Define a route for sending messages to the GPT-3.5 model
-@app.route('/send-message', methods=['POST'])
-def send_message():
-    try:
+
+#endpoint for creating user
+@app.route('/users', methods=['POST'])
+def create_users():
+    try :
         data = request.get_json()
-        user_message = data.get('message', '')
-
-
-         # Save user message to the database
-        with app.app_context():
-            user_history = ConversationHistory(message=f'User: {user_message}')
-            db.session.add(user_history)
-            db.session.commit()
-
-        # Append user message to the conversation history
-        # conversation_history.append(f'User: {user_message}')
-
-        with app.app_context():
-            conversation_history = [history.message for history in ConversationHistory.query.all()]
-
-        # Call OpenAI API to get chatbot response
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt='\n'.join(conversation_history),
-            max_tokens=150
-        )
-
-        # Extract the generated message from the OpenAI response
-        chatbot_response = response['choices'][0]['text'].strip()
-
-        # Append chatbot response to the conversation history
-        conversation_history.append(f'Chatbot: {chatbot_response}')
-        # print(conversation_history)
+        username = data.get('username','')
+        password = data.get('password','')
+        name = data.get('name','')
+        # print(data)
+        #Checking if user already exists
+        existing_user = Users.query.filter_by(username=username).first()
+        if existing_user : 
+            return jsonify({
+                'error':f'username: {username}, already exists!'
+            })
         
-        with app.app_context():
-            chatbot_history = ConversationHistory(message=f'Chatbot: {chatbot_response}')
-            db.session.add(chatbot_history)
-            db.session.commit()
+        #create a new user
+        new_user = Users(
+            username = username,
+            password = password,
+            name = name
+        )
+        db.session.add(new_user)
+        db.session.commit()
 
-        # return jsonify(conversation_history)
-        return jsonify(chatbot_response)
-
-    except Exception as e:
+        return jsonify({
+            'status':'user created successfully',
+            'name':new_user.name,
+            'user_id':new_user.id
+        })
+    #Exception handling
+    except Exception as ex:
         # Handle exceptions and return an error response
-        error_message = f"An error occurred: {str(e)}"
+        error_message = f"An error occurred: {str(ex)}"
         return jsonify({'error': error_message}), 500
 
 if __name__ == '__main__':
